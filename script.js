a39ecd67f253a7f434c8ee218c81af27c1eabf2f
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-05-24 13:42:15 +0300

    the application is ready

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.question').textContent = secretNumber;
+const displayGuessMessage = function(message) {
+    document.querySelector('.guess-message').textContent = message;
+}
 
-const eventHandler = () => {
+document.querySelector('.check').addEventListener('click', function () {
     const guessingNumber = Number(document.querySelector('.number-input').value);
 
+    // Нет введенного числа
     if (!guessingNumber) {
-        document.querySelector('.guess-message').textContent = 'Введите число!';
+        displayGuessMessage('Введите число!');
+
+    // Игрок победил
     } else if (guessingNumber === secretNumber) {
-        document.querySelector('.guess-message').textContent = 'Правильно!';
-    } else if (guessingNumber > secretNumber) {
+        displayGuessMessage('Правильно!');
+        document.querySelector('.question').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
+        document.querySelector('.question').style.width = '50rem';
 
-        if (score > 1) {
-            document.querySelector('.guess-message').textContent = 'Слишком много!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.guess-message').textContent = 'Игра завершена! Вы проиграли';
-            document.querySelector('.score').textContent = 0;
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
         }
-        
-    } else if (guessingNumber < secretNumber) {
 
-        if (scrore > 1) {
-            document.querySelector('.guess-message').textContent = 'Слишком мало!';
+    // Число пользователя неправильное
+    } else if (guessingNumber !== secretNumber) {
+        if (score > 1) {
+            displayGuessMessage(guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.guess-message').textContent = 'Игра завершена! Вы проиграли';
+            displayGuessMessage('Игра завершена! Вы проиграли');
             document.querySelector('.score').textContent = 0;
         }
     }
-};
+});
 
-document.querySelector('.check').addEventListener('click', eventHandler);
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    
+    document.querySelector('.question').textContent = '???';
+    document.querySelector('.question').style.width = '25rem';
+    displayGuessMessage('Начни угадывать!');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number-input').value = '';
+    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
+});
